9e919e24cdea5d4fc5f3d5f8a688696d5b48b798
GrafVorontsov <graf.vorontsov@gmail.com>
2018-06-04 01:09:31 +0300

    d1

--- src/tasks/Task2.java
+++ src/tasks/Task2.java
@@ -1,31 +1,6 @@
 package tasks;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-
-import static java.lang.Math.min;
-
-import java.io.*;
 import java.util.*;
-
-/**
- * Input
-
- s [the number of tests <= 10]
- n [the number of cities <= 10000]
- NAME [city name]
- p [the number of neighbors of city NAME]
- nr cost [nr - index of a city connected to NAME (the index of the first city is 1)]
- [cost - the transportation cost]
- r [the number of paths to find <= 100]
- NAME1 NAME2 [NAME1 - source, NAME2 - destination]
- [empty line separating the tests]
-
- Output
-
- cost [the minimum transportation cost from city NAME1 to city NAME2 (one per line)]
- */
 
 public class Task2 {
     private static final Graph.Edge[] GRAPH = {
